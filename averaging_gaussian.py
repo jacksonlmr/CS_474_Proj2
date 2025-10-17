2dc9cb70c620f40f5461db17a17d8d7d58e02685
jacksonlmr <38480226+jacksonlmr@users.noreply.github.com>
2025-10-17 22:13:08 +0000

    Changed gaussian to be simpler in accordance to how the assignment wants it to work.

--- averaging_gaussian.py
+++ averaging_gaussian.py
@@ -46,18 +46,53 @@ def average(input_img_array: np.ndarray, size: int):
 
 # print(average(average_test_array, 3))
 
-def gaussian(input_img_array: np.ndarray, std_dev: float):
-    #calculate size of kernel
-    size = int(np.ceil(5*std_dev))
+def gaussian(input_img_array: np.ndarray, weights: np.ndarray):
+    """
+    Computes the guassian of an image with a given mask.
 
-    #starting and ending coordinates to sample x and y
-    coord_start, coord_end = -(size-1)//2, (size-1)//2
-    
-    #generate 1d x, generate 1d y
-    x = y = np.linspace(coord_start, coord_end, size)
-    #outer product
+    **Parameters**
+    ---------------
+    >**input_img_array**:
+    >np.ndarray representing image. Should have dtype=np.uint8.
 
+    >**weights**:
+    >np.ndarray representing the mask to be used. Should have dtype=np.uint8.
+
+    **Returns**
+    -----------
+    >**output_array**: 2D array representing the guassian blurred image.
+    """
+    num_weights = weights.shape[0]*weights.shape[1]
+    weights = weights*(1/num_weights)
     return traverseImage(input_img_array, weights, weightSumMatrix)
 
-lenna_guassian = gaussian(lenna, 1.4)
+gaussian7 = np.array([
+    [1, 1, 2, 2, 2, 1, 1],
+    [1, 2, 2, 4, 2, 2, 1],
+    [2, 2, 4, 8, 4, 2, 2],
+    [2, 4, 8, 16, 8, 4, 2],
+    [2, 2, 4, 8, 4, 2, 2],
+    [1, 2, 2, 4, 2, 2, 1],
+    [1, 1, 2, 2, 2, 1, 1]
+])
+
+gaussian15 = np.array([
+    [2,2,3,4,5,5,6,6,6,5,5,4,3,2,2],
+    [2,3,4,5,7,7,8,8,8,7,7,5,4,3,2],
+    [3,4,6,7,9,10,10,11,10,10,9,7,6,4,3],
+    [4,5,7,9,10,12,13,13,13,12,10,9,7,5,4],
+    [5,7,9,11,13,14,15,16,15,14,13,11,9,7,5],
+    [5,7,10,12,14,16,17,18,17,16,14,12,10,7,5],
+    [6,8,10,13,15,17,19,19,19,17,15,13,10,8,6],
+    [6,8,11,13,16,18,19,20,19,18,16,13,11,8,6],
+    [6,8,10,13,15,17,19,19,19,17,15,13,10,8,6],
+    [5,7,10,12,14,16,17,18,17,16,14,12,10,7,5],
+    [5,7,9,11,13,14,15,16,15,14,13,11,9,7,5],
+    [4,5,7,9,10,12,13,13,13,12,10,9,7,5,4],
+    [3,4,6,7,9,10,10,11,10,10,9,7,6,4,3],
+    [2,3,4,5,7,7,8,8,8,7,7,5,4,3,2],
+    [2,2,3,4,5,5,6,6,6,5,5,4,3,2,2]
+])
+
+lenna_guassian = gaussian(lenna, gaussian7)
 cv2.imwrite(f'{outfile_save_path}lenna_guassian.jpg', lenna_guassian)
